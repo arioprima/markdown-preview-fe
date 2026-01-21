a7f41115c77d72cf1e5f3b705180796906534a99
arioprima <arioprima2@gmail.com>
2026-01-21 22:14:32 +0700

    feat: Add PM2 configuration for markdown-preview and update GitHub Actions workflow to deploy from prod-v1 branch to a new path.

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "markdown-preview",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      cwd: "/var/www/markdown-preview/markdown-preview-fe",
+      instances: 1,
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
+};
